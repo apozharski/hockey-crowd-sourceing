551ea5764285761d3573747d4f92c778c62b543f
Anton Pozharskiy <apozharski@gmail.com>
2017-10-02 10:44:54 -0400

    player model created

--- hockey/HDB/models.py
+++ hockey/HDB/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200)
+    draft_year = models.DateField()
